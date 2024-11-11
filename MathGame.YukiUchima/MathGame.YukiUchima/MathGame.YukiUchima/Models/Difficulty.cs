54504b82af5d4c37bfe208fdbf74137c0e21bd71
= <81452155+YukiUchima@users.noreply.github.com>
2024-11-10 23:11:41 -0700

    remove unused variables

---
+++ MathGame.YukiUchima/MathGame.YukiUchima/MathGame.YukiUchima/Models/Difficulty.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MathGame.YukiUchima.Models;
+
+internal class Difficulty
+{
+    public GameLevel Level { get; set; }
+}
+
+internal enum GameLevel
+{
+    Easy,
+    Medium,
+    Hard
+}
